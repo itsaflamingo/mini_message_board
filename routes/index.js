a78a8a039128e2601eab894a4090debf9e9a5de7
Liz <elizabeth.pinero.r@gmail.com>
2023-02-09 10:57:20 -0500

    messages are now displayed in home page from /new

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = []
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages, });
 });
+
+router.post('/new', function(req, res, next) {
+  const messageText = req.body.messageText;
+  const messageUser = req.body.messageUser;
+
+  messages.push({text: messageText, user: messageUser, added: new Date()});
+
+  res.redirect('/');
+})
 
 module.exports = router;
